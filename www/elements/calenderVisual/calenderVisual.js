5bd080ba965ad87e3663f685886a488f9494fbc8
Niloc <colinseelbach@gmail.com>
2022-10-27 16:41:02 +0200

    Added calender Element

---
+++ www/elements/calenderVisual/calenderVisual.js
@@ -0,0 +1,53 @@
+class CalenderVisual extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.renderCalender();
+    }
+    /**
+     * It fetches the HTML file, then it renders the HTML file, then it renders the lessons
+     */
+    renderCalender() {
+        fetch("../../elements/calenderVisual/calenderVisual.html").then((response) => {
+            response.text().then((text) => {
+                this.innerHTML = text;
+                for (let i = 0; i < 50; i++) {
+                    //TODO Render Lessons correctly by using Calender Class and Lesson Class values
+                    let placeholder = document.createElement("div");
+                    if (i < 5) {
+                        placeholder.classList.add("placeholder");
+                    }
+                    else {
+                        placeholder.classList.add("notUsed");
+                    }
+                    placeholder.appendChild(document.createElement("div"));
+                    placeholder.addEventListener("click", () => { this.renderInput(null); });
+                    this.querySelector(".body").appendChild(placeholder);
+                }
+            });
+        });
+    }
+    //TODO Uncomment Type, when Lesson class exists
+    renderInput(lesson /*: Lesson*/) {
+        fetch("../../elements/calenderVisual/calenderVisualInput.html").then((response) => {
+            response.text().then((text) => {
+                const visualInput = document.createElement("section");
+                visualInput.setAttribute("id", "hu-calender-input");
+                visualInput.innerHTML = text;
+                document.querySelector("body").insertBefore(visualInput, document.querySelector("main"));
+                document.querySelector("#hu-calender-input-save").addEventListener("click", () => { this.handelInputSubmit(); });
+                //TODO Render Data provided by the Lesson
+            });
+        });
+    }
+    handelInputSubmit() {
+        //TODO Safe Data in Lesson
+        this.removeInput();
+    }
+    removeInput() {
+        document.getElementById("hu-calender-input").remove();
+    }
+}
+window.customElements.define('hu-calender', CalenderVisual);
+//# sourceMappingURL=calenderVisual.js.map
---
+++ www/elements/calenderVisual/calenderVisual.ts
@@ -0,0 +1,59 @@
+class CalenderVisual extends HTMLElement{
+
+    constructor() {
+        super();
+    }
+    connectedCallback(){
+        this.renderCalender();
+    }
+
+    /**
+     * It fetches the HTML file, then it renders the HTML file, then it renders the lessons
+     */
+    private renderCalender(): void{
+        fetch("../../elements/calenderVisual/calenderVisual.html").then((response) => {
+            response.text().then((text) => {
+                this.innerHTML = text;
+
+                for (let i = 0; i < 50; i++) {
+                    //TODO Render Lessons correctly by using Calender Class and Lesson Class values
+                    let placeholder = document.createElement("div");
+                    if(i < 5) {
+                        placeholder.classList.add("placeholder")
+                    }else{
+                        placeholder.classList.add("notUsed")
+                    }
+                    placeholder.appendChild(document.createElement("div"))
+                    placeholder.addEventListener("click", () => {this.renderInput(null);})
+                    this.querySelector(".body").appendChild(placeholder);
+                }
+            })
+        });
+    }
+    //TODO Uncomment Type, when Lesson class exists
+    private renderInput(lesson/*: Lesson*/): void{
+        fetch("../../elements/calenderVisual/calenderVisualInput.html").then((response) => {
+            response.text().then((text: string) => {
+                const visualInput = document.createElement("section");
+                visualInput.setAttribute("id", "hu-calender-input");
+                visualInput.innerHTML = text;
+                document.querySelector("body").insertBefore(visualInput,document.querySelector("main"));
+
+                document.querySelector("#hu-calender-input-save").addEventListener("click", () => {this.handelInputSubmit();})
+                //TODO Render Data provided by the Lesson
+            });
+        });
+    }
+    private handelInputSubmit(): void{
+        //TODO Safe Data in Lesson
+
+        this.removeInput();
+    }
+
+    private removeInput():void {
+        document.getElementById("hu-calender-input").remove();
+
+    }
+
+}
+window.customElements.define('hu-calender', CalenderVisual);
